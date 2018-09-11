291ff657e260fa569cb3654f490d936a85180ba7
mynameiskate <katerina.akent@gmail.com>
2018-09-11 12:52:22 +0300

    Initial commit

---
+++ ThreadPoolLab/ThreadPoolLab/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadPoolLab
+{
+    class Program
+    {
+        private const string THREAD_PROMPT = "Type in amount of threads.";
+        private const string TASK_PROMPT = "Type in amount of tasks.";
+        private const string THREAD_ERROR_MSG = "The amount of threads should fall in range (0; ..].";
+        private const string TASK_ERROR_MSG = "The amount of tasks should fall in range (0; ..].";
+        private const string QUIT_PROMPT = "Type \"q\" to exit or any other key to continue...";
+        private const string QUIT_COMMAND = "q";
+
+        static void Main(string[] args)
+        {
+            string input = string.Empty;
+
+            while (input.Trim() != QUIT_COMMAND)
+            {
+
+                int threadCount = 0;
+                int taskCount = 0;
+
+                if (ReadNumber(out threadCount, THREAD_PROMPT, THREAD_ERROR_MSG)
+                    && ReadNumber(out taskCount, TASK_PROMPT, TASK_ERROR_MSG))
+                {
+                    using (var taskQueue = new TaskQueue(threadCount))
+                    {
+                        for (int i = 0; i < taskCount; i++)
+                        {
+                            taskQueue.EnqueueTask(ThreadPrint);
+                        }
+                    }
+                }
+
+                Console.WriteLine(QUIT_PROMPT);
+                input = Console.ReadLine();
+            }
+        }
+
+        private static bool ReadNumber(out int threadNumber, string promptMsg, string errorMsg)
+        {
+            Console.WriteLine(promptMsg);
+
+            bool parseResult = Int32.TryParse(Console.ReadLine(), out threadNumber)
+                && (threadNumber > 0);
+
+            if (!parseResult)
+            {
+                Console.WriteLine(errorMsg);
+            }
+
+            return parseResult;
+        }
+
+        private static void ThreadPrint()
+        {
+            int threadId = Thread.CurrentThread.ManagedThreadId;
+            Console.WriteLine($"Thread {threadId} started.");
+            Thread.Sleep(1000);
+            Console.WriteLine($"Thread {threadId} finished.");
+        }
+    }
+}
---
+++ ThreadPoolLab/ThreadPoolLab/TaskQueue.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace ThreadPoolLab
+{
+    public class TaskQueue : IDisposable
+    {
+        public delegate void TaskDelegate();
+
+        private ConcurrentQueue<TaskDelegate> _taskQueue = new ConcurrentQueue<TaskDelegate>();
+        private List<Thread> _threadPool = new List<Thread>();
+        private volatile bool _isDisposed = false;
+        private int _activeThreads;
+
+        public TaskQueue(int threadCount)
+        {
+            for (int i = 0; i < threadCount; i++)
+            {
+                var thread = new Thread(ThreadTask);
+                _threadPool.Add(thread);
+                thread.Start();
+            }
+        }
+
+        public bool EnqueueTask(TaskDelegate task)
+        {
+            if (!_isDisposed)
+            {
+                _taskQueue.Enqueue(task);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void ThreadTask()
+        {
+            while (true)
+            {
+                TaskDelegate task;
+
+                if (!_isDisposed)
+                {
+                    Interlocked.Increment(ref _activeThreads);
+                    if (_taskQueue.TryDequeue(out task))
+                    {
+                        task();
+                    }
+                    Interlocked.Decrement(ref _activeThreads);
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+
+        ~TaskQueue()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        protected void Dispose(bool disposing)
+        {
+            if (!_isDisposed && disposing)
+            {
+                while (_activeThreads != 0 || _taskQueue.Count != 0)
+                {
+
+                }
+                foreach (var thread in _threadPool)
+                {
+                    thread.Abort();
+                }
+
+                _isDisposed = true;
+            }
+        }
+    }
+}
