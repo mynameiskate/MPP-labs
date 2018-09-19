ab6c5811ebbd1358e3c604da693731f8fcb346c4
mynameiskate <katerina.akent@gmail.com>
2018-09-19 15:20:54 +0300

    File copying using thread pool added

---
+++ FileCopier/FileCopier/FileCopier.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using ThreadPoolLab;
+
+namespace FileCopier
+{
+    public class FileCopier
+    {
+        public event EventHandler<double> ProgressUpdated;
+        private long _bytesCopied;
+        private long _totalBytesCount;
+        private string _source;
+        private string _destination;
+        private int _copiedCount;
+
+        private double Progress
+        {
+            set
+            {
+                ProgressUpdated?.Invoke(this, value);
+            }
+        }
+
+        public int CopyFolder(string source, string destination)
+        {
+            if (!Directory.Exists(source))
+            {
+                throw new DirectoryNotFoundException("Source folder does not exist.");
+            }
+
+            if (!Directory.Exists(destination))
+            {
+                throw new DirectoryNotFoundException("Destination folder does not exist.");
+            }
+
+            _source = source;
+            _destination = destination;
+            CopyFilesAndFolders();
+            return _copiedCount;
+        }
+
+        private void CopySingleFolder(string path)
+        {
+            Directory.CreateDirectory(path.Replace(_source, _destination));
+        }
+
+        private void CopySingleFile(string path)
+        {
+            File.Copy(path, path.Replace(_source, _destination), true);
+
+            _bytesCopied += (new FileInfo(path)).Length;
+
+            ProgressUpdated?.Invoke(this, _bytesCopied / _totalBytesCount);
+            //Progress = _bytesCopied / _totalBytesCount;
+
+            Interlocked.Increment(ref _copiedCount);
+        }
+
+        private long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+            FileInfo[] fis = d.GetFiles();
+
+            foreach (FileInfo fi in fis)
+            {
+                size += fi.Length;
+            }
+
+            DirectoryInfo[] dis = d.GetDirectories();
+            foreach (DirectoryInfo di in dis)
+            {
+                size += DirSize(di);
+            }
+            return size;
+        }
+
+        private void CopyFilesAndFolders()
+        {
+            _bytesCopied = 0;
+            _totalBytesCount = DirSize(new DirectoryInfo(_source));
+
+            Progress = 0;
+
+            using (var taskQueue = new TaskQueue())
+            {
+                string[] directories = Directory.GetDirectories(_source, "*", SearchOption.AllDirectories);
+                foreach (string path in directories)
+                {
+                    taskQueue.EnqueueTask(() => CopySingleFolder(path));
+                }
+
+                string[] files = Directory.GetFiles(_source, "*", SearchOption.AllDirectories);
+                foreach (string path in files)
+                {
+                    taskQueue.EnqueueTask(() => CopySingleFile(path));
+                }
+            }
+        }
+    }
+}
---
+++ FileCopier/FileCopier/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.IO;
+
+namespace FileCopier
+{
+    class Program
+    {
+        private static ProgressBar _progressBar;
+        private const int ARG_COUNT = 2;
+
+        private static void OnProgressUpdated(object sender, double update)
+        {
+            _progressBar.Report(update);
+        }
+
+        static int Main(string[] args)
+        {
+            if (args.Length != ARG_COUNT)
+            {
+               Console.WriteLine("You should provide source and destination folders as parameters.");
+               return 1;
+            }
+
+            try
+            {
+                var fileCopier = new FileCopier();
+                _progressBar = new ProgressBar();
+                fileCopier.ProgressUpdated += OnProgressUpdated;
+                int copiedFiles = fileCopier.CopyFolder(args[0], args[1]);
+                Console.WriteLine();
+                Console.WriteLine($"Number of copied files: {copiedFiles}");
+                return 0;
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
+
+            return 0;
+        }
+    }
+}
