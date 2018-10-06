0a0dccfa3bc5de5fa261aba563f4c9dc3585de4d
mynameiskate <katerina.akent@gmail.com>
2018-10-06 22:23:24 +0300

    Assembly loader added

---
+++ AssemblyLab/AssemblyLab/AssemblyLoader.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace AssemblyLab
+{
+    public class AssemblyLoader
+    {
+        private string _assemblyPath;
+
+        public AssemblyLoader(string assemblyPath)
+        {
+            _assemblyPath = assemblyPath;
+        }
+
+        private Type[] LoadAssembly()
+        {
+            if(!File.Exists(_assemblyPath))
+            {
+                throw new FileNotFoundException($"Assembly cannot be located: {_assemblyPath}");
+            }
+
+            Assembly assembly = Assembly.LoadFile(_assemblyPath);
+
+            return assembly.GetTypes();
+        }
+
+        public MethodInfo[] GetPublicMethods()
+        {
+            Type[] assemblyTypes = LoadAssembly();
+
+            if (assemblyTypes == null)
+            {
+                return null;
+            }
+            else
+            {
+                return assemblyTypes
+                    .OrderByDescending(type => type.Namespace)
+                    .SelectMany(type => type.GetMethods())       
+                    .Where(method => method.IsPublic)
+                    .OrderByDescending(method => method.Name)
+                    .ToArray();
+            }
+        }
+
+    }
+}
---
+++ AssemblyLab/AssemblyLab/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Reflection;
+
+namespace AssemblyLab
+{
+    class Program
+    {
+        private const int ARG_COUNT = 1;
+
+        static int Main(string[] args)
+        {
+            if (args.Length != ARG_COUNT)
+            {
+                Console.WriteLine("You should provide pass to assembly as parameter.");
+                return 1;
+            }
+
+            try
+            {
+                var assemblyLoader = new AssemblyLoader(args[0]);
+                var methodInfos = assemblyLoader.GetPublicMethods();
+
+                PrintAssemblyMethodsInfo(methodInfos);
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return 1;
+            }
+        }
+
+        private static void PrintAssemblyMethodsInfo(MethodInfo[] methodInfos)
+        {
+            if (methodInfos == null)
+            {
+                Console.WriteLine("No public methods found.");
+            }
+            else
+            {
+                foreach (MethodInfo methodInfo in methodInfos)
+                {
+                    Console.WriteLine($"{methodInfo.DeclaringType.FullName}.{methodInfo.Name}");
+                }
+            }
+        }
+    }
+}
